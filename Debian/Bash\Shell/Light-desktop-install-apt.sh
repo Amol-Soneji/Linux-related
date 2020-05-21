7e0df4caf025bbfbb12639b038fa592c5a3af83f
Amol Soneji <eon747@live.com>
2020-05-21 01:24:18 -0400

    Create Light-desktop-install-apt.sh
    Created an installation script that will install a very light weight desktop environment.  The script will install xterm, lxpanel, and either mutter or metacity based on the Debian environment.

---
+++ Debian/Bash\Shell/Light-desktop-install-apt.sh
@@ -0,0 +1,8 @@
+#Install light weght desktop components.  This shell script utilizes 'apt'.  
+sudo apt-get install xterm lxpanel;
+
+#Once this step is done  check to see if mutter will work in the environment, as some specialised non-standard debian environments do not support mutter.  
+
+#  -----> if else statements to be placed here.  <------
+
+
