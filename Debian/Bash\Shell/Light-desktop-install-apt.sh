ae71f61fe1b695206df63a34174387a43a58d666
Amol Soneji <eon747@live.com>
2020-05-25 01:54:28 -0400

    Update Light-desktop-install-apt.sh
    Worked on this a bit more.  Added the commands that come after the installation process.

--- Debian/Bash\Shell/Light-desktop-install-apt.sh
+++ Debian/Bash\Shell/Light-desktop-install-apt.sh
@@ -1,8 +1,25 @@
-#Install light weght desktop components.  This shell script utilizes 'apt'.  
+#Install required components for the light weght desktop components.  This shell script utilizes 'apt'.  
+#term might not be available thus xterm is installed as well.  
 sudo apt-get install xterm lxpanel;
 
 #Once this step is done  check to see if mutter will work in the environment, as some specialised non-standard debian environments do not support mutter.  
 
 #  -----> if else statements to be placed here.  <------
+
+if  [ $DISPLAY -eq 'vnc']
+    
+
+#After installation
+
+if  [ $InstallType -eq  1]
+then
+    echo "exec lxpanel" >> ~/.xinitrc
+    echo "exec mutter" >> ~/.xinitrc
+else
+    echo "exec lxpanel" >> ~/.xinitrc
+    echo "exec metacity" >> ~/.xinitrc
+fi
+
+echo "Installation completed.  Enter the command startx or xinit to start the light desktop.  ";  
 
 
