5730061055973eb942c6da0d7bb26fe86ebddcd0
Amol Soneji <eon747@live.com>
2020-09-21 03:11:04 -0400

    Update Light-desktop-install-apt.sh
    Added a note stating that the script is assuming that it was invoked with a "sudo".

--- Debian/Bash\Shell/Light-desktop-install-apt.sh
+++ Debian/Bash\Shell/Light-desktop-install-apt.sh
@@ -1,3 +1,4 @@
+#It is assumed that this script was invoked with a command that had "sudo".  Otherwise a password prompt will show, or an error will show if the ivoking user is not in the sudoers file.  
 #Install light weght desktop components.  This shell script utilizes 'apt-get'.  
 sudo apt-get install xterm lxpanel;
 
