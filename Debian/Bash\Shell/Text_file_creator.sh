c9b0c409d8aaecf066c001741c730352a891d556
Amol Soneji <eon747@live.com>
2021-02-12 23:39:46 -0500

    Create Text_file_creator.sh
    Created a program that creates a file, and writes content to it, that is entered at prompts in the program.

---
+++ Debian/Bash\Shell/Text_file_creator.sh
@@ -0,0 +1,17 @@
+echo "To exit just enter 'EXIT'  ";
+echo "Press enter for every line that you want to add to a file.   ";
+echo "Enter name of file to be created.  :  ";
+read file_name;
+echo "Enter line:  ";
+read file_line;
+echo "$file_line" >> "$file_name";
+while [ 1 -eq 1 ]; do
+  echo "Enter next line:  ";
+  read file_line;
+  if [[ "$file_line" = "EXIT" ]]; then
+    break
+  fi
+  echo "$file_line" >> "$file_name";
+done
+echo "File successfully created wiht the following content.  ";
+cat "$file_name";
